dc5fa97fd2a002e740f5d86fc3872f1b30663447
cp <877734642@qq.com>
2021-01-06 16:58:26 +0800

    第3次提交

--- src/function/Function1.java
+++ src/function/Function1.java
@@ -1,7 +1,29 @@
+package function;
+
+import java.util.Optional;
+import java.util.stream.Stream;
+
 /**
  * @Author cp
  * @CreateDate 2021/1/5
  * @Email soul_thought@163.com
  */
 public class Function1 {
+    public static void main(String[] args) {
+        Stream<String> names = Stream.of("Lamurudu", "Okanbi", "Oduduwa");
+
+        Optional<String> longest = names
+
+                .filter(name -> name.startsWith("L"))
+
+                .findFirst();
+
+        longest.ifPresent(name -> {
+
+            String s = name.toUpperCase();
+
+            System.out.println("The longest name is "+ s);
+
+        });
+    }
 }
